08c2cf43af4ec0571c866755374fd12372947df0
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-21 18:32:07 +0300

    controller index add method create

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -7,4 +7,7 @@ class Index
     public function index(){
         echo 'index controller index action';
     }
+    public function create(){
+        echo 'index controller and create name ';
+    }
 }
