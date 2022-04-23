94c34d9bb8638a4e7f1eda543ab123093b21469f
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-23 11:00:40 +0300

    controller index remove unuseble

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -7,10 +7,5 @@ class Index
     public function index(){
         echo 'index controller index action';
     }
-    public function create(){
-        echo 'index controller and create name ';
-    }
-    public function store(){
-        echo 'index controller and store name ';
-    }
+
 }
