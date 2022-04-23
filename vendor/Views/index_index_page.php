b060d14854576f9b0b4152b64703b224d1599863
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-23 11:58:39 +0300

    views default template and index_index_page

---
+++ vendor/Views/index_index_page.php
@@ -0,0 +1,7 @@
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda eum ex ipsum libero molestiae porro. Debitis illo
+    libero recusandae rem tempore? Cupiditate et iste officiis quae ratione saepe, veritatis! Accusantium ad animi
+    consectetur consequatur, consequuntur eaque eligendi, exercitationem ipsam iure laboriosam libero magnam maiores
+    minus molestiae molestias mollitia necessitatibus quaerat quam quasi qui, quia quibusdam recusandae soluta ut vitae!
+    A autem dolor dolorum explicabo fugiat magni molestiae mollitia pariatur possimus qui quis reiciendis suscipit
+    tempore unde voluptas, voluptate voluptatibus voluptatum?</p>
