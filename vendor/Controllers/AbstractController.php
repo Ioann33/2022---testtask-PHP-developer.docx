1d9aa5028497cb3decfd82ee0202a325e9f718ff
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-27 21:52:04 +0300

    AbstractController

---
+++ vendor/Controllers/AbstractController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Controllers;
+
+use Core\View;
+
+abstract class AbstractController
+{
+    /**
+     * @var View
+     */
+    protected $view;
+
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    abstract public function index();
+}
