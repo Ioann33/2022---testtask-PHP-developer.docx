1a6c18f2fcbfd4c9ed3a5d6570a5f4b9e1988242
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-29 12:37:22 +0300

    controller index action

--- vendor/Controllers/Note.php
+++ vendor/Controllers/Note.php
@@ -5,11 +5,16 @@ namespace Controllers;
 
 
 use Core\View;
+use Models\NoteModel;
 
 class Note extends AbstractController
 {
     public function index()
     {
-
+        $model = new NoteModel();
+        $notes = $model->all();
+        $this->view->render('note_index_page', [
+            'notes'=>$model->all(),
+        ]);
     }
 }
