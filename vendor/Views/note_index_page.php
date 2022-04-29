018766adbffafdaa945fa643acfc318658458c55
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-04-29 21:35:04 +0300

    add action button delete and update in view

--- vendor/Views/note_index_page.php
+++ vendor/Views/note_index_page.php
@@ -14,7 +14,19 @@
                     <tr>
                         <td><?= $note['id']?></td>
                         <td><?= $note['text']?></td>
-                        <td></td>
+                        <td>
+                            <div class="button">
+                                <form method="post" action="<?=Route::url('note', 'update')?>">
+                                    <input type="hidden" value="<?= $note['text']?>" name="oldNote">
+                                    <input type="hidden" value="<?= $note['id']?>" name="index">
+                                    <input type="submit" value="update">
+                                </form>
+                                <form method="post" action="<?=Route::url('note', 'delete')?>">
+                                    <input type="hidden" value="<?= $note['id']?>" name="index">
+                                    <input type="submit" value="delete">
+                                </form>
+                            </div>
+                        </td>
                     </tr>
                 <?php endforeach;?>
             <?php endif;?>
